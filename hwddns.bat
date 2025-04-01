04947d97610379067adbdcc49ab50a775030208a
loecom <154944277+loecom@users.noreply.github.com>
2025-04-01 19:36:09 +0800

    Delete hwddns.bat

--- hwddns.bat
+++
@@ -1,2 +0,0 @@
-C:\Users\loeco\go\hwddns\hwddns.exe -ak GJXLEJRZ7MNXNVWDVLW1 -sk g31jsjzedeT6bh9H6zztyCOrdaimzQTRPVgF18De -rm w -dm zjlchb.com -ur https://ww.zjlchb.com:{STUN_Web_PORT}
-C:\Users\loeco\go\hwddns\hwddns.exe -ak GJXLEJRZ7MNXNVWDVLW1 -sk g31jsjzedeT6bh9H6zztyCOrdaimzQTRPVgF18De -rm l -dm zjlchb.com -ur https://ll.zjlchb.com:{STUN_Web_PORT}
